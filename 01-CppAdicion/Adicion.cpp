c91e978879d06b164e6ea236b1a1bedb480890ef
Nsanguineti <Nahusanguineti@gmail.com>
2020-04-29 20:50:28 -0300

    Add files via upload

---
+++ 01-CppAdicion/Adicion.cpp
@@ -0,0 +1,21 @@
+/*
+    Adición.
+    Objetivo: se ingresan dos números y se muestra en pantalla la suma de éstos.
+    Nahuel Sanguineti
+    20200429
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a, b;
+    cout << "Ingrese un número: ";
+    cin >> a;
+    cout << "Ingrese otro número: ";
+    cin >> b;
+    
+    cout << "La adición de " << a << " + " << b << " es: " << a+b << endl << endl;
+    
+}
